7d3a6f34b2b12837f5c8f8f4970001064fbd380e
boyhub <38520868+boyhub@users.noreply.github.com>
2020-10-10 01:12:52 +0530

    python

---
+++ Day1_B29.py
@@ -0,0 +1,6 @@
+Python 3.10.0a1 (v3.10.0a1:8e9afaf822, Oct  5 2020, 14:09:56) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('Hello World')
+Hello World
+>>> 
